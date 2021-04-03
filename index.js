d4f0d1f3663bf8497cca2b79f66b1897c2b55d6e
Panos Astithas <pastithas@google.com>
2021-04-03 01:23:54 +0000

    First cut at getting spec stats

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 'use strict';
 
 const browserSpecs = require('browser-specs');
-const {Octokit} = require('@octokit/rest');
 const fetch = require('node-fetch');
+const { Octokit } = require('@octokit/rest');
+const { retry } = require("@octokit/plugin-retry");
+const { throttling } = require("@octokit/plugin-throttling");
+const MyOctokit = Octokit.plugin(retry, throttling);
 
-const octokit = new Octokit({
+const octokit = new MyOctokit({
   auth: process.env.GH_TOKEN,
+  userAgent: 'past/spec-stats',
+  throttle: {
+    onRateLimit: (retryAfter, options) => {
+      octokit.log.warn(
+        `Request quota exhausted for request to ${options.url}`
+      );
+      octokit.log.warn(
+        `Retry#${options.request.retryCount + 1} after ${retryAfter} seconds!`
+      );
+      return true;
+    },
+    onAbuseLimit: (retryAfter, options) => {
+      // Don't retry, only log an error.
+      octokit.log.warn(`Abuse detected for request to ${options.url}!`);
+    },
+  },
 });
 
 const SINCE = '2021-01-01T00:00:00Z';
 const VENDORS = ['Apple', 'Google', 'Microsoft', 'Mozilla'];
 const SPEC_DOMAIN = 'spec.whatwg.org/';
 const WHATWG_ENTITIES = 'https://raw.githubusercontent.com/whatwg/participant-data/main/entities.json';
+const responseTimes = new Map();
 
 function listRepositories() {
   const repoSet = new Set();
@@ -55,16 +75,16 @@ function listEvents(options, issue) {
   return octokit.paginate(listOptions);
 }
 
-async function isVendorMember(username) {
+async function getVendorMember(username) {
   for (const org of VENDORS) {
     try {
       await octokit.orgs.checkPublicMembershipForUser({ org, username });
       console.debug(`${username} is a member of ${org}`);
-      return true;
+      return org;
     } catch (e) { /* Ignore */ }
   }
   console.debug(`${username} is not a member of a browser vendor`);
-  return false;
+  return "unknown";
 }
 
 function getVendorMap() {
@@ -82,12 +102,19 @@ function getVendorMap() {
   });
 }
 
+function addDelay(org, delay) {
+  if (!responseTimes.has(org)) {
+    responseTimes.set(org, []);
+  }
+  const delays = responseTimes.get(org);
+  delays.push(delay);
+}
+
 async function main() {
   const repos = listRepositories();
 
   // Get the GH orgs for the vendors.
   const vendorOrgs = await getVendorMap();
-  console.debug(vendorOrgs);
 
   for (const repo of repos) {
     const options = optionsFromURL(repo);
@@ -99,10 +126,10 @@ async function main() {
     const issues = await listIssues(options);
     for (const issue of issues) {
       // Only users from browser vendors count.
-      if (!await isVendorMember(issue.user.login)) {
+      const vendor = await getVendorMember(issue.user.login);
+      if (!vendor) {
         continue;
       }
-
       const events = await listEvents(options, issue);
       const firstInterestingEvent = events.find((event) => {
         // Ignore events that don't have a timestamp.
@@ -117,19 +144,32 @@ async function main() {
         if (event.actor.login === issue.user.login) {
           return false;
         }
-        // TODO: filter to only repo maintainers, but how?
+
         return true;
       });
       if (firstInterestingEvent) {
         const t0 = Date.parse(issue.created_at);
         const t1 = Date.parse(firstInterestingEvent.created_at);
         const delay = Math.round((t1 - t0) / (24 * 3600 * 1000));
+        addDelay(vendor, delay);
         console.log(issue.html_url, `first activity after ${delay} day(s)`);
       } else {
         console.log(issue.html_url, 'no activity');
       }
     }
   }
+
+  for (const vendor of responseTimes.keys()) {
+    const times = responseTimes.get(vendor);
+    const vendorAvgDelay = times.reduce((a, b) => a + b) / times.length;
+    console.log(`Average delay for ${vendor} issues is ${vendorAvgDelay}`);
+  }
+  const totalTimes = [];
+  for (const times of responseTimes.values()) {
+    totalTimes.concat(times);
+  }
+  const totalAvgDelay = totalTimes.reduce((a, b) => a + b) / totalTimes.length;
+  console.log(`Total average delay ${totalAvgDelay}`);
 }
 
 if (require.main === module) {
